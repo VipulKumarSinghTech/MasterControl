5f9b2023e5eae69942a75b369bd7cddbfd925909
Vipul Kumar Singh <vipul24.9669@gmail.com>
2022-09-25 22:58:31 +0530

    Initial Commit

---
+++ src/main/java/com/github/VipulKumarSinghTech/MasterControl/MasterControlApplication.java
@@ -0,0 +1,13 @@
+package com.github.VipulKumarSinghTech.MasterControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/VipulKumarSinghTech/MasterControl/MasterControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.VipulKumarSinghTech.MasterControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
